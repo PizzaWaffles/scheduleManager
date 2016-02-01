17cd2af1d9b6ac476ee451001c34ba5f5da93c69
Daniel Blevins <danielb2cool@gmail.com>
2016-02-01 15:47:02 -0700

    fixed fatal flaw in with root

--- manager.sh
+++ manager.sh
@@ -5,11 +5,10 @@ sourceCalendar="HACH Global Calendar"
 destCalendar="Work Schedule"
 searchKey="Daniel"
 
-
 number="545619" #number used to find events created by 'scheduleManager'
-tmpFolder="$workingDirtmp/"
-fileName="$tmpFolderagenda.data"
-logFile="$tmpFoldercurrentLog.log"
+tmpFolder=$workingDir"tmp/"
+fileName=$tmpFolder"agenda.data"
+logFile=$tmpFolder"currentLog.log"
 
 mkdir $tmpFolder
 touch $fileName
@@ -29,7 +28,7 @@ while read line; do
 		month=$(echo $line | cut -c6-7)
 		day=$(echo $line | cut -c9-10)
 		text=$(echo $line | cut -c12-)
-		text=${text//"Daniel"/""}
+		#text=${text//"Daniel"/""}
 		if [ "$year" -ge "$curYear" ] && [ "$month" -ge "$curMonth" ] && [ "$day" -ge "$curDay" ]; then
 			echo "Adding----"$line >> $logFile
 			gcalcli --calendar "$destCalendar" --title "$text" --when "$month/$day/$year" --duration 1 --where "Work" --description "$number" --reminder 180 add --allday >> $logFile
